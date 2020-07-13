6f09b6335aada85649c790e659d0a5aade550358
Nagao Shuhei <nash.peisan969@gmail.com>
2020-07-13 13:17:59 +0900

    ゲストログイン機能を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,4 +43,11 @@ class User < ApplicationRecord
   def has_written?(article)
     articles.exists?(id: article.id)
   end
+
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+    end
+  end
 end
